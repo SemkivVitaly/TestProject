4ae0bb7152f64f465dfb2c23fcdf74ac0a582b20
MSI\vital <>
2026-03-13 00:23:50 +0300

    Add .pio and .vscode (build and IDE config)

---
+++ _New/esp32s3box_wifi_bridge/esp32s3box_wifi_bridge/.pio/build/esp32s3box/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32s3box/FrameworkArduino/firmware_msc_fat.c.o: \
+ C:/Users/vital/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ C:/Users/vital/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
